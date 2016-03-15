d30ee2ee6df7b8521875b370c86ff29ad2188420
Dominic Garms <djgarms@gmail.com>
2016-03-15 10:45:44 +0800

    fix of client side script

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'planettraining:angular-file-saver-bundle',
-  version: '1.1.0',
+  version: '1.1.0_1',
   // Brief, one-line summary of the package.
   summary: 'angular file saver meteor package',
   // URL to the Git repository containing the source code for this package.
@@ -14,5 +14,5 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('angular:angular@1.4.8', ['client']);
   api.use('ecmascript');
-  api.addFiles('bower_components/angular-file-saver/dist/angular-file-saver.bundle.js');
+  api.addFiles('bower_components/angular-file-saver/dist/angular-file-saver.bundle.js','client');
 });
